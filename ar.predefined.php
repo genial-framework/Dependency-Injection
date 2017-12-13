63a50f9252e259c596f14309f9096ddef33cc3cd
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-12 21:35:54 -0500

    Update ar.predefined.php

--- ar.predefined.php
+++ ar.predefined.php
@@ -4,5 +4,4 @@
  * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
  * @license   <https://github.com/Genial-Framework/Dependency-Injection/blob/master/LICENSE> New BSD License
  */
-
 define('GENIAL_DEPENDENCY_INJECTION_VERSION', '1.0.0');
