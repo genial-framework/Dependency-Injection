b2779dd99b0cd05e8e082fea69cfd2b98206928f
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-12 21:16:40 -0500

    Create ar.predefined.php

---
+++ ar.predefined.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * @link      <https://github.com/Genial-Framework/Dependency-Injection> for the canonical source repository
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Dependency-Injection/blob/master/LICENSE> New BSD License
+ */
+ 
+define('GENIAL_DEPENDENCY_INJECTION_VERSION', '1.0.0');
