4fe031b0a490e60dad1ee56eb769ed3c234415bf
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-14 15:59:28 -0500

    Update Container.php

--- src/Container.php
+++ src/Container.php
@@ -1,1 +1,15 @@
+<?php
+/*
+ * @link      <https://github.com/Genial-Framework/Dependency-Injection> for the canonical source repository
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Dependency-Injection/blob/master/LICENSE> New BSD License
+ */
 
+namespace Genial\Dependency_Injection;
+
+/**
+ * Container
+ */
+class Container
+{ 
+}
